29048de0947489b5efcbda715fe2052d150d9aa9
SERGO <sergei181989@gmail.com>
2021-01-22 20:41:22 +0200

    Create new project "Notes" and working.
    Создание нового проекта по урокам Notes и изучение по видео-урокам.

---
+++ notes/src/components/Message.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="message">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      message: {
+        type: String,
+        require: true
+      }
+    }
+
+  }
+</script>
+
+<style lang="scss" scoped>
+  .message {
+    text-align: center;
+  }
+  p {
+    color: cornflowerblue;
+  }
+</style>
---
+++ notes/src/components/NewNote.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="new-note">
+    <label for="title">Title</label>
+    <input v-model="note.title" type="text" id="title">
+    <label for="descr">Description</label>
+    <textarea v-model="note.descr" id="descr"></textarea>
+    <button class="btn btnPrimary" @click="addNote">New note</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      note: {
+        type: Object,
+        require: true
+      }
+    },
+    methods: {
+      addNote () {
+        this.$emit('addNote', this.note)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .new-note {
+    text-align: center;
+  }
+</style>
