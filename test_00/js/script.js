bdb877b91d3a263014a3a8ce097dcf04c34f2e21
SERGO <sergei181989@gmail.com>
2021-01-22 20:40:18 +0200

    Study base vue.js
    Изучение базового синтаксиса и начальных примеров по vue.js

--- test_00/js/script.js
+++ test_00/js/script.js
@@ -1,8 +1,25 @@
 const app = new Vue({
   el: '#app',
   data: {
-    title: 'Input form',
-    name: '',
-    email: '',
+    title: 'Изучение Vue.js',
+    showContent: false,
+    user: {
+      name: 'Alex',
+      email: 'info@mail.net'
+    },
+    users: [
+      {name: 'Jack', email: 'jack@mail.net', age: 22},
+      {name: 'Alex', email: 'alex@mail.net', age: 23},
+      {name: 'Scott', email: 'scott@mail.net', age: 24},
+    ]
   },
+  methods: {
+    getContent () {
+      this.showContent = !this.showContent
+      console.log(this.user);
+    }
+  },
+  computed: {
+
+  }
 });
