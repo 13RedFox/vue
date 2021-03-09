62d74a614be54b4ce33ebc97834b1a82872ff4b1
13redfox <sergei181989@gmail.com>
2021-03-10 00:40:36 +0200

    feat: add project hello-world & learning vue-v.3 in youtube course

---
+++ hello-world/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>{{ greet }} {{ name }}</h1>
+  <div v-text="channel"></div>
+  <div v-html="hack"></div>
+  <h2 v-bind:id="headingId">Heading</h2>
+  <button v-bind:disabled="isDisabled">Bind</button>
+</template>
+
+<script>
+
+	export default {
+		name: 'App',
+		data() {
+			return {
+				greet: 'Hello',
+				name: 'Sergei',
+				channel: 'Codevolution',
+				hack: `<a href="#" onclick="alert('You have been hacked!')">Win a prize!</a>`,
+        headingId:  'heading',
+        isDisabled: false,
+			}
+		},
+		components: {}
+	}
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
